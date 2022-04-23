e53c8f4515f25985c1d73557a50f1aa6f9c69e4a
Osmell Caicedo <correo.oele@gmail.com>
2022-04-22 21:07:53 -0500

    first commit

---
+++ data.php
@@ -0,0 +1,22 @@
+<?php
+
+$enlaces = [
+	 'primero',
+	 'segundo',
+	 'tercero'
+	];
+	
+	
+$portafolio = [
+	// el primer post con su atributos
+	1 => [
+		'titulo' => 'titulo',
+		'descripcion' => 'descripcion',
+		'boton' => 'boton'
+	],
+	2 => [
+		'titulo' => 'titulo 2',
+		'descripcion' => 'descripcion 2',
+		'boton' => 'boton 2 '
+	]
+];
